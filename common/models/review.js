e03e9f899416592d9009e27b03a4cdb806f82663
Alex Plescan <alexpls@gmail.com>
2016-04-04 21:03:00 +1000

    Add beforeRemote hook to Review model

--- common/models/review.js
+++ common/models/review.js
@@ -1,3 +1,10 @@
 module.exports = function(Review) {
+  'use strict'
 
+  Review.beforeRemote('create', function(context, user, next) {
+    let req = context.req;
+    req.body.date = Date.now()
+    req.body.publisherId = req.accessToken.userId
+    next()
+  })
 };
